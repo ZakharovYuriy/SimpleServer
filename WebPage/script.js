6d7016201e9893b276b3297a49d76f45d88a2f6b
ZakharovYuriy <zakharov.yura13@yandex.ru>
2023-01-31 22:53:11 +0100

    start adding magnet

--- WebPage/script.js
+++ WebPage/script.js
@@ -4,6 +4,8 @@ window.onresize = resize;
 var pageWidth = document.documentElement.clientWidth
 var pageHeight = document.documentElement.clientHeight
 
+var scrolling = false;
+
 //обработка изменения размера окна
 function resize(event) {
 pageWidth = document.documentElement.clientWidth
@@ -12,15 +14,63 @@ pageHeight = document.documentElement.clientHeight
 }
 //вращение фона при перемещении мыши
 function move (event) {
-	let x = event.clientX - pageWidth / 2;
+	//rotation
+	/*let x = event.clientX - pageWidth / 2;
 	let y = pageHeight / 2 - event.clientY;
 	var deg = 0 - (180 / Math.PI ) * Math.acos(x/Math.sqrt(x*x+y*y));
 	//console.log(x + ':' + y + ' deg:' + deg);
 	
 	if(y<0){
 		deg = -deg;
-	}
+	}*/
+	
+	//only left-right moution
+	let x = event.clientX - pageWidth / 2;
+	var deg = (180/pageWidth * x)/3;
 	
 	let all_doc = document.getElementById('all_doc');
 	all_doc.style.backgroundImage = 'linear-gradient(' +deg +'deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)';
 }
+
+var scroll_zone = 0;
+
+window.addEventListener('scroll', function() {
+	var position = window.scrollY;
+	
+	let temp_pos;
+	if(position < 400){
+		temp_pos = 0;
+	}else if(position >= 400 && position <= 1000){
+		temp_pos = 1;
+	}else{
+		temp_pos = 2;
+	}
+	
+	if(scroll_zone != temp_pos){
+		scroll_zone=temp_pos;
+		console.log(position + "scroll_zone :" +scroll_zone);
+		let elem = document.getElementById('about_me');
+		console.log(elem.getBoundingClientRect());
+		/*window.scrollBy({
+			  top: 200,
+			  left: 0,
+			  behavior: 'smooth'
+			});*/
+	}
+	
+	
+	/*if(position > 400 && position<1000){
+		window.scrollBy({
+			  top: 1,
+			  left: 0,
+			  behavior: 'smooth'
+			});
+	}*/
+
+});
+
+
+
+
+
+
